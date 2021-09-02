ff7052ca86648cf5c60039ae7e0ada0c5f0c55f8
Yosup <yosupc49@gmail.com>
2021-09-01 21:52:43 -0700

    javascript_file_add

---
+++ src/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var hw = document.getElementById('test');
+    hw.addEventListener('click', function (){
+            document.write("hello world");
+        }
+    )
+}
